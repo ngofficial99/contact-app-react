c76e4951006ea65919a4e3903c32fa052416f9d2
Nishant Gupta <ngofficial99@gmail.com>
2024-07-02 19:43:28 +0530

    created contact list component

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
+import ContactList from "./ContactList"
 
 function App() {
   return (
-    <div>
+    <div className="ui container">
       <Header />
       <AddContact />
+      <ContactList/>
+
     </div>
   );
 }
