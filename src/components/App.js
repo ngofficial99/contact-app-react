8b7440fecab27dff258afcce2c7806e98e923814
Nishant Gupta <ngofficial99@gmail.com>
2024-07-02 19:20:34 +0530

    added the header and add contact form

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
-import ContactList from "./ContactList";
 
 function App() {
   return (
     <div>
       <Header />
       <AddContact />
-      <ContactList />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    return(
+
+        <div className="ui fixed menu">
+            <div className="ui container center aligned">
+            <h2 style={{ textAlign: 'center' }}>Contact Manager</h2>
+
+            </div>
+        </div>
+    );
+
+};
+
+export default Header;
