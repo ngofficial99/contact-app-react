5e04f6af78edcc1f552385c61809f463c8ed3f7e
Nishant Gupta <ngofficial99@gmail.com>
2024-07-02 21:38:13 +0530

    use state and props for the contact list done

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,15 @@ import ContactList from "./ContactList"
 
 function App() {
   const [contacts, setContacts] = useState([])
+
+  const addContactHandler =(contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]);
+  }
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler} />
       <ContactList contact={contacts}/>
 
     </div>
