6006374c137bcecdcce734f0a025efb0afe2f688
Nishant Gupta <ngofficial99@gmail.com>
2024-07-02 20:27:22 +0530

    added the contact list and the contact card

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,28 @@ import AddContact from "./AddContact";
 import ContactList from "./ContactList"
 
 function App() {
+   const contacts = [
+    {
+      id :"1",
+      "name" :"Nishant",
+      "email" :"Nishant@email.com"
+    },
+    {
+      id :"2",
+      "name" :"Vishal",
+      "email" :"vishal@email.com"
+    },
+    {
+      id :"3",
+      "name" :"rakesh",
+      "email" :"rakesh@email.com"
+    }
+   ]
   return (
     <div className="ui container">
       <Header />
       <AddContact />
-      <ContactList/>
+      <ContactList contact={contacts}/>
 
     </div>
   );
