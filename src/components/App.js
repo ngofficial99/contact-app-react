17b44a008a4853204e3e73f9ec8d03fc26c46537
Nishant Gupta <ngofficial99@gmail.com>
2024-07-08 22:49:11 +0530

    fetching contacts from the api

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,17 @@ import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 import ContactDetail from "./ContactDetail";
+import api from "../api/contacts"
 
 function App() {
   const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
+
+  //RetriveContacts
+  const retrieveContacts = async () => {
+    const response = await api.get("/contacts")
+    return response.data
+  }
 
   const addContactHandler = (contact) => {
     console.log(contact);
@@ -25,8 +32,13 @@ function App() {
   };
 
   useEffect(() => {
-    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (retriveContacts) setContacts(retriveContacts);
+    // const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    // if (retriveContacts) setContacts(retriveContacts);
+    const getAllContacts = async () => {
+      const allContacts = await retrieveContacts();
+      if(allContacts) setContacts(allContacts)
+    }
+    getAllContacts()
   }, []);
 
   useEffect(() => {
